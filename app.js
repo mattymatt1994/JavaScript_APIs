a3899456e94b3439145f87bf8e6d053c1617936c
mattymatt1994 <jedimatt.jordan@gmail.com>
2023-05-30 22:08:13 -0400

    Fetch API

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 console.log("Hello World!\n==========\n");
 
+let search = document.getElementById("submitSearch");
+let input = document.getElementById("searchWord");
+let image = document.querySelector("img");
+let feedBack = document.getElementById("feedback");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+search.addEventListener("click", () => {
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=0qtCVud0XXO4Fk0CtxuYFxir8uV06HcZ` + input.value, 
+    {mode: "cors"}
+  )
+  .then((res) => 
+  res.json())
+  .then((res) => {
+    feedBack.textContent = "";
+    image.src = res.data.images.original.url;
+    input.value = "";
+  })
+  .catch((err) => {
+    console.error(err);
+    feedBack.textContent = err.message;
+  });
+});
